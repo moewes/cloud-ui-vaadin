f36e11aa5fb9dfd21bf101fef8b92cf05fe07b85
maik <moewes@users.noreply.github.com>
2020-12-17 09:54:25 +0100

    setup build workflow first components

---
+++ src/main/java/net/moewes/cloud/ui/vaadin/TextField.java
@@ -0,0 +1,31 @@
+package net.moewes.cloud.ui.vaadin;
+
+import lombok.Getter;
+import net.moewes.cloud.ui.UiComponent;
+
+@Getter
+public class TextField extends UiComponent {
+
+  private String placeholder;
+  private String label;
+  private boolean required;
+
+  public TextField() {
+    super("vaadin-text-field");
+  }
+
+  public void setLabel(String label) {
+    this.label = label;
+    getElement().setAttribute("label", label);
+  }
+
+  public void setPlaceholder(String placeholder) {
+    this.placeholder = placeholder;
+    getElement().setAttribute("placeholder", placeholder);
+  }
+
+  public void setRequired(boolean required) {
+    this.required = required;
+    getElement().setAttribute("required",null);
+  }
+}
