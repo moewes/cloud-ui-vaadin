ef45c54465be1ca2ae988a72e27dcfe29a435f64
moewes <moewes@users.noreply.github.com>
2023-06-10 19:29:54 +0000

    Automated API website updates.

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"Alle Packages","u":"allpackages-index.html"},{"l":"net.moewes.cloudui.vaadin"},{"l":"net.moewes.cloudui.vaadin.input"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"net.moewes.cloudui.vaadin"},{"l":"net.moewes.cloudui.vaadin.input"}]
